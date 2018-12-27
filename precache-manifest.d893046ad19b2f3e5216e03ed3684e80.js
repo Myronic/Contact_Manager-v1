fd822288d99562f7f03334d5857715fccea9df4e
Myron Carvalho <myron.c@somaiya.edu>
2018-12-27 23:59:27 +0530

    Updates

---
+++ precache-manifest.d893046ad19b2f3e5216e03ed3684e80.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "10bfff799aa2bcbfff4b",
+    "url": "/Contact_Manager-v1/static/js/runtime~main.10bfff79.js"
+  },
+  {
+    "revision": "e5d0f8f180526ccf9fa5",
+    "url": "/Contact_Manager-v1/static/js/main.e5d0f8f1.chunk.js"
+  },
+  {
+    "revision": "a19d67789cbba05c4f0f",
+    "url": "/Contact_Manager-v1/static/js/1.a19d6778.chunk.js"
+  },
+  {
+    "revision": "a19d67789cbba05c4f0f",
+    "url": "/Contact_Manager-v1/static/css/1.805b331b.chunk.css"
+  },
+  {
+    "revision": "ee7d3405b472ff6bbdae19986061c63c",
+    "url": "/Contact_Manager-v1/index.html"
+  }
+];
