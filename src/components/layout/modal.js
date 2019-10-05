1f4f4e3ca92464339935a057f32b0eda9f84162b
rajs80266 <40178180+rajs80266@users.noreply.github.com>
2019-10-05 23:44:42 +0530

    added modal.js file

---
+++ src/components/layout/modal.js
@@ -0,0 +1,23 @@
+<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
+  Launch demo modal
+</button>
+<!-- Modal -->
+<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">WELCOME TO LITHUB!</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <p>It's free, and always will be!</p>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">DISMISS</button>
+        <button type="button" class="btn btn-primary">ACCEPT</button>
+      </div>
+    </div>
+  </div>
+</div>
