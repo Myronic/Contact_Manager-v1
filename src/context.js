8588cb8444f508ce962b6abda39da2b57bb1576c
kaustoobhwalunj <56836218+kaustoobhwalunj@users.noreply.github.com>
2019-10-21 20:47:24 +0530

    added modal and fixed bugs

--- src/context.js
+++ src/context.js
@@ -2,7 +2,31 @@ import React, { Component } from "react";
 import axios from "axios";
 
 const Context = React.createContext();
+<!-- Button trigger modal -->
+<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
+  Launch demo modal
+</button>
 
+<!-- Modal -->
+<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        ...
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
 const reducer = (state, action) => {
   switch (action.type) {
     case "DELETE_CONTACT":
@@ -63,4 +87,4 @@ export class Provider extends Component {
   }
 }
 
-export const Consumer = Context.Consumer;
+
