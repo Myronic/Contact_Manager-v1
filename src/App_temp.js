20169d3765ff6b27cd3a6479cdd2d6e4bf07ce84
mahimagupta29 <43781052+mahimagupta29@users.noreply.github.com>
2020-10-03 23:38:23 +0530

    code cleanup

--- src/App_temp.js
+++ src/App_temp.js
@@ -3,25 +3,7 @@ import "./App.css";
 
 class App extends React.Component {
   render() {
-    // const name = " myronic";
-    // const num1 = 23;
-    // const num2 = 43;
-    // const flag = false;
-    // let math;
-    // if (flag) {
-    //   math = (
-    //     <h4>
-    //       {num1}+{num2}= {num1 + num2}
-    //     </h4>
-    //   );
-    // } else {
-    //   math = null;
-    // }
-
-    //with jsx
-    /*
-      NOTE: ALL SCRIPTING IS DONE OUTSIDE THE RETURN FN.
-    */
+  
     return (
       <div className="App">
         <h1>The app component hello</h1>
